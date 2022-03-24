9cfa6a8ac749f267d2d18042b2e3e55c3b70bb90
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2022-03-24 22:32:09 +0800

    增加state內容

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,36 @@
 import { createStore } from 'vuex'
+import { calNowTime } from "@/assets/js/calNowTime";
 
 export default createStore({
   state: {
+    mag: {
+      min: 3,
+      max: 10
+    },
+    depth: {
+      min: 0,
+      max: 1000
+    },
+    location: {
+      W: 120,
+      E: 126,
+      S: 20,
+      N: 26
+    },
+    date: {
+      start: "",
+      end: ""
+    }
   },
   getters: {
+    calTime(state) {
+      const dd = calNowTime();
+      state.date.start = dd.day_90;
+      state.date.end = dd.todayUTC;
+    },
   },
   mutations: {
+
   },
   actions: {
   },
