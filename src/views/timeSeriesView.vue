8227e932a8c81de227e702b9caf4381b9d8679fc
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2022-03-27 22:30:27 +0800

    echarts test

--- src/views/timeSeriesView.vue
+++ src/views/timeSeriesView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="timeSeries">
     <h1>This is an timeSeries page</h1>
+    <testUI />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import testUI from "@/components/test.vue";
+
+
+export default defineComponent({
+  name: "timeSeriesView",
+  components: {
+    testUI,
+
+  },
+});
+</script>
+
